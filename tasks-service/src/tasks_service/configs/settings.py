cc0116b280a19f3cd502c9751e4cf7434a4048a0
novoselovsv <novoselovsv@bk.ru>
2025-11-12 13:02:42 +0300

    Remake to poetry

---
+++ tasks-service/src/tasks_service/configs/settings.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class DBSettings(BaseModel):
+    dialect: str = 'postgresql'
+    driver: str = 'asyncpg'
+    name: str = 'tasks'
+    username: str = 'general_user'
+    password: str = 'general_user_pass'
+    host: str = 'localhost'
+    port: int = 5432
+
+    @property
+    def dsn(self):
+        return (f'{self.dialect}+{self.driver}://'
+                f'{self.username}:{self.password}'
+                f'@{self.host}:{self.port}/{self.name}')
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_nested_delimiter='__')
+
+    debug: bool = True
+    user_secret: str = 'SECRET'
+    db: DBSettings = DBSettings()
